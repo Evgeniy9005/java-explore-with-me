7343c5cb6ffe11dd808447b73c000a65e9dbcc57
Евгений <Evgeniy_90_05@mail.ru>
2024-08-08 15:18:12 +1100

    начал писать junit тесты на AdminController

--- ewm-service/src/test/java/ru/practicum/admin/AdminControllerTest.java
+++ ewm-service/src/test/java/ru/practicum/admin/AdminControllerTest.java
@@ -6,24 +6,27 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import ru.practicum.category.dto.CategoryDto;
 import ru.practicum.category.dto.NewCategoryDto;
+import ru.practicum.constants.State;
 import ru.practicum.data.Controller;
 import ru.practicum.events.EventsController;
 import ru.practicum.stats.Stats;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,17 +34,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AdminControllerTest extends Controller {
 
 
-     Map<Integer,CategoryDto> categoryDtoMap;
+     static Map<Integer,CategoryDto> categoryDtoMap;
 
     @BeforeEach
      void setUp() {
-        categoryDtoMap = getCategoryDtoMap(2);
+        initUser(2);
+        initCategory(2);
+        initEventWithParam(5);
     }
 
     @Test
     void addNewCategory() throws Exception {
-
-
         when(adminService.addNewCategory(any(NewCategoryDto.class),any())).thenReturn(categoryDtoMap.get(1));
 
             mvc.perform(post("/admin/categories")
@@ -52,22 +55,55 @@ class AdminControllerTest extends Controller {
                     .andDo(print())
                     .andExpect(status().isCreated());
         verify(adminService).addNewCategory(any(),any());
-
-    }
-
-
-
-    @Test
-    void deleteCategory() {
-
     }
 
     @Test
-    void upCategory() {
+    void deleteCategory() throws Exception {
+        doNothing().when(adminService).deleteCategory(anyInt(),any(HttpServletRequest.class));
+        mvc.perform(delete("/admin/categories/1"))
+                .andExpect(status().isNoContent());
+        verify(adminService).deleteCategory(anyInt(),any(HttpServletRequest.class));
+
+        mvc.perform(delete("/admin/categories/-1"))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    void getEvents() {
+    void upCategory() throws Exception {
+        when(adminService.upCategory(any(CategoryDto.class),anyInt(),any())).thenReturn(categoryDtoMap.get(1));
+
+        mvc.perform(patch("/admin/categories/1")
+                        .content(objectMapper.writeValueAsString(categoryDtoMap.get(1)))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+        verify(adminService).upCategory(any(),anyInt(),any());
+    }
+
+    @Test
+    void getEvents() throws Exception {
+
+        when(adminService.getEvents(any(),any(),any(),anyString(),anyString(),anyInt(),anyInt(),any()))
+                .thenReturn(eventFullDtoList);
+
+        mvc.perform(get("/admin/events")
+                        .content(objectMapper.writeValueAsString(eventFullDtoList))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                .param("users", getUsersIdParam())
+                .param("states",State.PUBLISHED.toString())
+                .param("categories","1,2")
+                .param("paid","true")
+                .param("rangeStart","2024-11-30 15:10:05")
+                .param("rangeEnd","2024-12-31 15:10:05")
+                .param("from","0")
+                .param("size","100"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        verify(adminService).getEvents(any(),any(),any(),anyString(),anyString(),anyInt(),anyInt(),any());
     }
 
     @Test
--- ewm-service/src/test/java/ru/practicum/data/Controller.java
+++ ewm-service/src/test/java/ru/practicum/data/Controller.java
@@ -19,17 +19,22 @@ import ru.practicum.category.model.Category;
 import ru.practicum.client.StatsClient;
 import ru.practicum.compilations.converter.CompilationMapper;
 import ru.practicum.compilations.converter.CompilationMapperImpl;
+import ru.practicum.compilations.dto.CompilationDto;
 import ru.practicum.compilations.model.Compilation;
 import ru.practicum.events.EventsService;
 import ru.practicum.events.coverter.EventsMapper;
 import ru.practicum.events.coverter.EventsMapperImpl;
+import ru.practicum.events.dto.EventFullDto;
+import ru.practicum.events.dto.EventShortDto;
 import ru.practicum.events.model.Event;
 import ru.practicum.stats.Stats;
 import ru.practicum.users.converter.UserMapper;
 import ru.practicum.users.converter.UserMapperImpl;
+import ru.practicum.users.dto.UserDto;
 import ru.practicum.users.model.User;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -40,6 +45,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.practicum.data.Data.generationData;
+import static ru.practicum.data.Data.printList;
 
 public class Controller {
 
@@ -58,30 +65,102 @@ public class Controller {
     @Autowired
     protected MockMvc mvc;
 
-    protected static List<Category> categoryList;
+    protected List<Category> categoryList;
 
-    protected static CategoryMapper categoryMapper = new CategoryMapperImpl();
+    protected List<CategoryDto> categoryDtoList = new ArrayList<>();
 
-    protected static List<User> userList;
+    protected Map<Integer,CategoryDto> categoryDtoMap;
+
+    protected CategoryMapper categoryMapper = new CategoryMapperImpl();
+
+    protected List<User> userList;
+
+    protected List<UserDto> userDtoList = new ArrayList<>();
+
+    protected Map<Integer, UserDto> userDtoMap;
 
     protected UserMapper userMapper = new UserMapperImpl();
 
-    protected static List<Event> eventList;
+    protected List<Event> eventList;
+
+    protected List<EventFullDto> eventFullDtoList = new ArrayList<>();
+
+    protected List<EventShortDto> eventShortDtoList = new ArrayList<>();
+
+    protected Map<Integer,EventFullDto> eventFullDtoMap;
+
+    protected Map<Integer,EventShortDto> eventShortDtoMap;
 
     protected EventsMapper eventsMapper = new EventsMapperImpl();
 
-    protected static List<Compilation> compilationList;
+    protected List<Compilation> compilationList;
+
+    protected List<CompilationDto> compilationDtoList = new ArrayList<>();
+
+    protected Map<Integer,CompilationDto> compilationDtoMap;
 
     protected CompilationMapper compilationMapper = new CompilationMapperImpl();
 
-    protected static Map<Integer,CategoryDto> getCategoryDtoMap(Integer createObjects) {
+    protected void initCategory(Integer createObjects) {
+        categoryList = generationData(createObjects,Category.class);
+        printList(categoryList,"ccc");
 
-        categoryList = Data.generationData(createObjects,Category.class);
-        Data.printList(categoryList,"ccc");
-
-        return  categoryList.stream()
+        categoryDtoMap = categoryList.stream()
                 .map(category -> categoryMapper.toCategoryDto(category))
                 .collect(Collectors.toMap(c -> c.getId(),c -> c));
+
+        categoryDtoList.addAll(categoryDtoMap.values());
+    }
+
+    protected void initUser(Integer createObjects) {
+        userList = generationData(createObjects,User.class);
+        printList(userList);
+
+        userDtoMap = userList.stream()
+                .map(user -> userMapper.toUserDto(user))
+                .collect(Collectors.toMap(UserDto::getId,u -> u));
+
+        userDtoList.addAll(userDtoMap.values());
+    }
+
+    protected void initEventWithParam(Integer createObjects) {
+        eventList = generationData(createObjects,Event.class,userList.get(0),categoryList.get(0));
+        printList(eventList);
+
+        eventFullDtoMap = eventList.stream()
+                .map(event -> eventsMapper.toEventFullDto(event))
+                .collect(Collectors.toMap(EventFullDto::getId,eventFullDto -> eventFullDto));
+
+        eventShortDtoMap = eventList.stream()
+                .map(event -> eventsMapper.toEventShortDto(event))
+                .collect(Collectors.toMap(EventShortDto::getId,eventShortDto -> eventShortDto));
+
+        eventFullDtoList.addAll(eventFullDtoMap.values());
+        eventShortDtoList.addAll(eventShortDtoMap.values());
+    }
+
+    protected String getUsersIdParam() throws Exception {
+        return String.valueOf(userList.stream().map(User::getId)
+                .collect(Collectors.toList()))
+                .replace("[","").replace("]","");
+    }
+
+    protected String getCategoryIdParam() throws Exception {
+        return objectMapper.writeValueAsString(
+                categoryList.stream().map(Category::getId).collect(Collectors.toList())
+        );
+    }
+
+    protected String toJson(Object o) throws Exception {
+        return objectMapper.writeValueAsString(o);
+    }
+
+    protected List<CategoryDto> getCategoryDtoList() {
+        return categoryDtoList;
+    }
+
+    protected Map<Integer,CategoryDto> getCategoryDtoMap() {
+        return categoryDtoMap;
     }
 
     protected RequestPostProcessor request() {
